163ddb8642cfc7c11bf9970c52b9b773381f4682
Adejoh Godwin <59458336+Adejoh-godwin@users.noreply.github.com>
2022-07-25 08:01:26 +0100

    first commit

---
+++ resources/views/layouts/title-meta.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8" />
+<title>@@title | Dason - Admin & Dashboard Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="Zoidinc - Vendor Admin" name="description" />
+<meta content="Themesdesign" name="author" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
---
+++ storage/framework/views/be35e000bc859c6b380acb85dda1c7f651fbde80.php
@@ -0,0 +1,2 @@
+<body data-topbar="dark">
+<?php /**PATH /Applications/MAMP/htdocs/laravel/zoidinc admin/resources/views/layouts/body.blade.php ENDPATH**/ ?>
